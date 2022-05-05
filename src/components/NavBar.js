9cfda6862ce601aa28af305694a3011d64d74416
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-05 13:37:58 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkItems = links.map((link,index)=>{
+
+    let refLink = '#'+link
+  
+    return <a key={index} href={refLink}>{link}</a>
+  })
+
+  return(
+    <nav>
+      <ul>
+        <li>
+          {linkItems}
+        </li>
+      </ul>
+    </nav>
+  )
+  
+   
 }
 
 export default NavBar;
